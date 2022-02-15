107b719956490914d52352dad4d63ea91fd4f63b
Marwan Tarek <mrwantarik50@gmail.com>
2022-02-15 20:03:49 +0200

    layout update

--- js/quote.js
+++ js/quote.js
@@ -29,31 +29,32 @@ if(query.has('mql') && query.has('cpo') && query.has('cpd') &&  query.has('month
   if(result_toggler_status == 'true'  && lead_toggler_status == 'false' && payment_toggler_status == 'false'){
 
       // Lead Generation  with payment plan OFF
+      //render graphs and setting data
+      graphs(ratio , closeRatio , cac , phone , email);
+
       // Add Table row 
       add_row("Marketing Qualified Lead" , `$${price}` , 1 , `$${price}`);
       document.getElementById('total').textContent =  `$${price}`;
-      //render graphs and setting data
-      graphs(ratio , closeRatio,lv , cac , phone , email);
-      $('#eventsBox').css("display" , 'none');
-      $('#mql-row').css("display" , 'block');
-      $('#mql').val(`$${price}`);
 
   }else if(result_toggler_status == 'true'  && lead_toggler_status == 'false' && payment_toggler_status == 'true'){
 
      // Lead Generation  with payment plan ON
-     add_row("Marketing Qualified Lead Payment Plan Pricing" , `$${mql_payment}` , payment_months , `$${price}`);
-    document.getElementById('total-title').textContent =  `${payment_months} Payments of`;
-    document.getElementById('total').textContent =  `$${mql_payment}`;
+
     //render graphs and setting data
-    graphs(ratio , closeRatio,lv , cac , phone , email);
-    $('#eventsBox').css("display" , 'none');
-    $('#mql-row').css("display" , 'block');
-    $('#mql').val(`$${mql_payment}`);
+    graphs(ratio , closeRatio , cac , phone , email);
+
+     add_row("Marketing Qualified Lead Payment Plan Pricing" , `$${mql_payment}` , payment_months , `$${price}`);
+    document.getElementById('x-payments').textContent =  `${payment_months} Payments of $${mql_payment}`;
+    document.getElementById('total').textContent =  `$${price}`;
 
   }else if (result_toggler_status == 'true'  && lead_toggler_status == 'true' && payment_toggler_status == 'false'){
 
       
       // Sales Closing  with payment plan OFF 
+
+      //render graphs and setting data
+      graphs(ratio , closeRatio , cac , phone , email);
+
       //calc mql  * 6 
       let months_total_price = (Number(price.replace(',','')) * 6).toLocaleString(undefined, { maximumFractionDigits: 2 }) ;
       //calc total payment and closer comission
@@ -62,15 +63,15 @@ if(query.has('mql') && query.has('cpo') && query.has('cpd') &&  query.has('month
       add_row("Marketing Qualified Lead" , `$${price}`  , 6 , `$${months_total_price}` );
       add_row("Closed Deal" , `$${closer_comission}` , 1 , `$${closer_comission}`);
       document.getElementById('total').textContent = `$${total}`  ;
-      //render graphs and setting data
-      graphs(ratio , closeRatio,lv , cac , phone , email);
-      $('#eventsBox').css("display" , 'none');
-      $('#mql-row').css("display" , 'block');
-      let total_landed_deal = cac ;
-      $('#mql').val(`$${total_landed_deal}`);
+      
   }else if (result_toggler_status == 'true'  && lead_toggler_status == 'true' && payment_toggler_status == 'true'){
 
     // Sales Closing  with payment plan ON
+
+    //render graphs and setting data
+    graphs(ratio , closeRatio, cac , phone , email);
+
+
     let mql_months = (Number(mql_payment.replace(',','')) * payment_months).toLocaleString(undefined, { maximumFractionDigits: 2 }); 
     for( let i = 0; i < 20 ; i++){
       add_row("Marketing Qualified Lead Payment Plan" , `$${mql_payment}`  , payment_months , `$${mql_months}` );
@@ -79,38 +80,33 @@ if(query.has('mql') && query.has('cpo') && query.has('cpd') &&  query.has('month
     let closer_months = (Number(closer.replace(',','')) * payment_months).toLocaleString(undefined, { maximumFractionDigits: 2 }); 
     add_row("Closed Sale Payment Plan" , `$${closer}` , payment_months , `$${closer_months}`);
     //calc total payment and closer 
-    let total = (Number(closer.replace(',','')) +  ( Number(mql_payment.replace(',','')) * 20)).toLocaleString(undefined, { maximumFractionDigits: 2 }); 
-    document.getElementById('total-title').textContent =  `${payment_months} Payments of`;
+    let total = ((Number(mql_months.replace(',','')) * 20 ) +  Number(closer_months.replace(',',''))).toLocaleString(undefined, { maximumFractionDigits: 2 }); 
+    let total_payments = (Number(closer.replace(',','')) +  ( Number(mql_payment.replace(',','')) * 20)).toLocaleString(undefined, { maximumFractionDigits: 2 }); 
+    document.getElementById('x-payments').textContent =  `${payment_months} Payments of $${total_payments}`;
     document.getElementById('total').textContent =  `$${total}`;
-    //render graphs and setting data
-    graphs(ratio , closeRatio,lv , cac , phone , email);
-    $('#eventsBox').css("display" , 'none');
-    $('#mql-row').css("display" , 'block');
-    $('#mql').val(`$${landed_deal}`);
+    
   }else if (result_toggler_status == 'false'  && lead_toggler_status == 'false' && payment_toggler_status == 'false') {
     // Priced Per Task
+    //render graphs and setting data
+    graphs(ratio , closeRatio , cac , phone , email);
+
     let total_cost_per_dial =  (Number(cost_per_dial.replace(',','')) * 400).toLocaleString(undefined, { maximumFractionDigits: 2 })  ;
     let total_cost_per_open =  (Number(cost_per_open.replace(',','')) * 100).toLocaleString(undefined, { maximumFractionDigits: 2 })  ;
-    add_row("Cost Per Dial" , `$${cost_per_dial}` , 400 , `$${total_cost_per_dial}`);
-    add_row("Cost Per Open" , `$${cost_per_open}` , 100 , `$${total_cost_per_open}`);
-    let total = (Number(total_cost_per_dial.replace(',','')) + Number(total_cost_per_open.replace(',',''))).toLocaleString(undefined, { maximumFractionDigits: 2 });  
-    document.getElementById('total').textContent =  `$${total}`;
-    //render graphs and setting data
-    graphs(ratio , closeRatio,lv , cac , phone , email);
-    $('#mql-row').css("display" , 'none');
-    $('#eventsBox').css("display" , 'block');
-
+    
     if ($('#email').is(":checked") && $('#phone').is(":checked")){
       
-      $('#phoneDials').text(`$${cost_per_dial}`);
-      $('#emailOpens').text(`$${cost_per_open}`);
-
+      add_row("Cost Per Dial" , `$${cost_per_dial}` , 400 , `$${total_cost_per_dial}`);
+      add_row("Cost Per Open" , `$${cost_per_open}` , 100 , `$${total_cost_per_open}`);
+      let total = (Number(total_cost_per_dial.replace(',','')) + Number(total_cost_per_open.replace(',',''))).toLocaleString(undefined, { maximumFractionDigits: 2 });  
+      document.getElementById('total').textContent =  `$${total}`;
     }else if ($('#email').is(":checked")){
-      $('#phoneDials').text(`N/A`);
-      $('#emailOpens').text(`$${cost_per_open}`);
+      add_row("Cost Per Dial" , `N/A` , `N/A` , `N/A`);
+      add_row("Cost Per Open" , `$${cost_per_open}` , 100 , `$${total_cost_per_open}`);
+      document.getElementById('total').textContent =  `$${total_cost_per_open}`;
     }else {
-      $('#phoneDials').text(`$${cost_per_dial}`);
-      $('#emailOpens').text(`N/A`);
+      add_row("Cost Per Dial" , `$${cost_per_dial}` , 400 , `$${total_cost_per_dial}`);
+      add_row("Cost Per Open" , `N/A` , `N/A` , `N/A`);
+      document.getElementById('total').textContent =  `$${total_cost_per_dial}`;
     }
     
   }
@@ -119,7 +115,23 @@ if(query.has('mql') && query.has('cpo') && query.has('cpd') &&  query.has('month
 }else{
   console.log("there are a missing query");
 } 
-function graphs(ratio , closeRatio,lv , cac , phone , email) {
+function Campaign_Description () {
+  if ($('#email').is(":checked") && $('#phone').is(":checked")){
+    //set Compaign Description 
+    $('#description').text('Phone & E-mail Campaign');
+
+  }else if ($('#email').is(":checked")){
+    // set Compaign Description 
+    $('#description').text('E-mail Campaign Only');
+
+  }else {
+    // set Compaign Description 
+    $('#description').text('Telemarketing Campaign Only');
+
+  }
+}
+
+function graphs(ratio , closeRatio , cac , phone , email) {
   let graph = document.querySelectorAll('.graph');
   let p_checkBox = document.getElementById('phone');
   let e_checkBox = document.getElementById('email');
@@ -146,10 +158,9 @@ function graphs(ratio , closeRatio,lv , cac , phone , email) {
     graph[1].style.width = `${closeRatio}%`;
   }
   graph[1].textContent = `${closeRatio}%`;
-  
-  graph[2].style.width = `${lv}%`;
-  graph[2].textContent = `${lv}%`;
+  Campaign_Description ()
 }
+
 function add_row(disc , price , qty , total){
   let tableRows = document.createElement('tr');
   tableRows.innerHTML = `
